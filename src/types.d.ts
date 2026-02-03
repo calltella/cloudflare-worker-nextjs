cc1a212e71c67211553aed6ec297458bdb5aaec8
calltella <yuichi.asa@gmail.com>
2026-02-03 03:45:49 +0000

    simple code

--- src/types.d.ts
+++
@@ -1,6 +0,0 @@
-import { WorkerEntrypoint } from "cloudflare:workers";
-
-export declare class API extends WorkerEntrypoint {
-  async fetch(req: Request): Promise<Response>;
-  async add(a: number, b: number): Promise<number>;
-}
